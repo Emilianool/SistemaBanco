85a4c8cbeea879b2b93d5311d5b35aea7f28f67f
Emiliano <Emiliano.oliveira@virtus.ufcg.edu.br>
2018-03-13 11:16:11 -0300

    Adicionando Testes

---
+++ src/test/java/Projeto/Exemplo/SistemaBanco/GerenciadoraClientesTest_Ex5.java
@@ -0,0 +1,70 @@
+package Projeto.Exemplo.SistemaBanco;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class GerenciadoraClientesTest_Ex5 {
+
+	private GerenciadoraClientes gerClientes;
+
+	@Test
+	public void testPesquisaCliente() {
+
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		int idCLiente01 = 1;
+		int idCLiente02 = 2;
+		Cliente cliente01 = new Cliente(idCLiente01, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(idCLiente02, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente01);
+		clientesDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+
+		/* ========== Execução ========== */
+		Cliente cliente = gerClientes.pesquisaCliente(idCLiente01);
+		
+		/* ========== Verificações ========== */
+		assertThat(cliente.getId(), is(idCLiente01));
+		
+	}
+	
+	@Test
+	public void testRemoveCliente() {
+
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		int idCLiente01 = 1;
+		int idCLiente02 = 2;
+		Cliente cliente01 = new Cliente(idCLiente01, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(idCLiente02, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente01);
+		clientesDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+		
+		/* ========== Execução ========== */
+		boolean clienteRemovido = gerClientes.removeCliente(idCLiente02);
+		
+		/* ========== Verificações ========== */
+		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		assertNull(gerClientes.pesquisaCliente(idCLiente02));
+		
+	}
+	
+}
+
---
+++ src/test/java/Projeto/Exemplo/SistemaBanco/GerenciadoraContasTest_Ex6.java
@@ -0,0 +1,72 @@
+package Projeto.Exemplo.SistemaBanco;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class GerenciadoraContasTest_Ex6 {
+
+	private GerenciadoraContas gerContas;
+	
+	@Test
+	public void testTransfereValor() {
+
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		int idConta01 = 1;
+		int idConta02 = 2;
+		ContaCorrente conta01 = new ContaCorrente(idConta01, 200, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		
+		gerContas = new GerenciadoraContas(contasDoBanco);
+
+		/* ========== Execução ========== */
+		boolean sucesso = gerContas.transfereValor(idConta01, 100, idConta02);
+		
+		/* ========== Verificações ========== */
+		assertTrue(sucesso);
+		assertThat(conta02.getSaldo(), is(100.0));
+		assertThat(conta01.getSaldo(), is(100.0));
+	}
+	
+	@Test
+	public void testTransfereValor_SaldoInsuficiente() {
+
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		int idConta01 = 1;
+		int idConta02 = 2;
+		ContaCorrente conta01 = new ContaCorrente(idConta01, 100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		
+		gerContas = new GerenciadoraContas(contasDoBanco);
+
+		/* ========== Execução ========== */
+		boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+		
+		/* ========== Verificações ========== */
+		assertTrue(sucesso);
+		assertThat(conta01.getSaldo(), is(-100.0));
+		assertThat(conta02.getSaldo(), is(200.0));
+	}
+
+}
